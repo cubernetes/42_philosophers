156182f8ac275c1be5d19591926cc7485bfe7e5f
cubernetes <tischmid@proton.me>
2024-07-20 22:29:16 +0200

    Remove disallowed files

--- philo/philosophers-visualizer/README.md
+++
@@ -1,9 +0,0 @@
-# 42 Philosophers visualizer
-
-### Usages :
-
-`./philo <args> | ./visualizer [-s step]` **Using pipe, the philo program must stop by itself.**
-
-`./visualizer [-s step] < philo_output.txt`
-
-The step option modifies the minimum step displayed on the ruler.
--- philo/source_me.sh
+++
@@ -1,8 +0,0 @@
-# raw
-x () { make -s && (set -o pipefail && _cleanup () { ret=$? ; trap - EXIT ; [ $ret = 0 ] && { sed 's/\x1B\[[0-9;]*[A-Za-z]//g' philo_output | xsel --clipboard --input ; sed 's/\x1B\[[0-9;]*[A-Za-z]//g' philo_output | ./philosophers-visualizer/visualizer ; } ; 1>/dev/null rm -f philo_output; } && trap _cleanup EXIT && 1>/dev/null rm -f philo_output && time stdbuf -o0 ./philo "${@}" | tee philo_output ; _cleanup) || echo Cannot compile; }
-
-# helgrind
-y () { make -s && (set -o pipefail && _cleanup () { ret=$? ; trap - EXIT ; [ $ret = 0 ] && { sed 's/\x1B\[[0-9;]*[A-Za-z]//g' philo_output | xsel --clipboard --input ; sed 's/\x1B\[[0-9;]*[A-Za-z]//g' philo_output | ./philosophers-visualizer/visualizer ; } ; 1>/dev/null rm -f philo_output; } && trap _cleanup EXIT && 1>/dev/null rm -f philo_output && time stdbuf -o0 valgrind --tool=helgrind ./philo "${@}" | tee philo_output ; _cleanup) || echo Cannot compile; }
-
-# drd
-z () { make -s && (set -o pipefail && _cleanup () { ret=$? ; trap - EXIT ; [ $ret = 0 ] && { sed 's/\x1B\[[0-9;]*[A-Za-z]//g' philo_output | xsel --clipboard --input ; sed 's/\x1B\[[0-9;]*[A-Za-z]//g' philo_output | ./philosophers-visualizer/visualizer ; } ; 1>/dev/null rm -f philo_output; } && trap _cleanup EXIT && 1>/dev/null rm -f philo_output && time stdbuf -o0 valgrind --tool=drd ./philo "${@}" | tee philo_output ; _cleanup) || echo Cannot compile; }
